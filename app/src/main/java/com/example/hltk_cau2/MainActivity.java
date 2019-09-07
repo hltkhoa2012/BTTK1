fb789cc8a09fceb8f05ab7c69fe689a031735780
Khoa <hltkhoa2012@gmail.com>
2019-09-07 09:35:11 +0700

    add button thoat

--- app/src/main/java/com/example/hltk_cau2/MainActivity.java
+++ app/src/main/java/com/example/hltk_cau2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.hltk_cau2;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnThoat = (Button)findViewById(R.id.btn_Thoat);
+        btnThoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Exit");
+                builder.setMessage("Good Bye: Khoa !!!");
+                builder.setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i){
+                    finish();
+
+                }
+            });
+	        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i){
+                    dialogInterface.dismiss();
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            }
+        });
     }
 }
